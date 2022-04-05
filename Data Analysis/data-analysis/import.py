a3dc691ba51d1ed064c632e86d6568e7e9c0c335
LeonLukaschek <leon.lukaschek@gmail.com>
2022-04-05 19:06:53 +0200

    Update import.py
    Einzelplots für alle Datenquellen angelegt, Grid erweitert

--- Data Analysis/data-analysis/import.py
+++ Data Analysis/data-analysis/import.py
@@ -9,18 +9,80 @@ data = np.loadtxt(file, delimiter=",", dtype=float)
 time, pressure, height, temperatureBMP, temperatureMPU, AcX, AcY, AcZ, GyX, GyY, GyZ, UV = np.hsplit(data, 12)
 
 # Plot height and pressure against time
-plot1 = plt.subplot2grid((2, 1), (0, 0))
-plot2 = plt.subplot2grid((2, 1), (1, 0))
+plotHeight = plt.subplot2grid((3, 4), (0, 0))
+plotPressure = plt.subplot2grid((3, 4), (1, 0))
+plotUV = plt.subplot2grid((3, 4), (2, 0))
+plotTempBMP = plt.subplot2grid((3, 4), (0, 1))
+plotTempMPU = plt.subplot2grid((3, 4), (1, 1))
+plotAcX = plt.subplot2grid((3, 4), (0, 2))
+plotAcY = plt.subplot2grid((3, 4), (1, 2))
+plotAcZ = plt.subplot2grid((3, 4), (2, 2))
+plotGyX = plt.subplot2grid((3, 4), (0, 3))
+plotGyY = plt.subplot2grid((3, 4), (1, 3))
+plotGyZ = plt.subplot2grid((3, 4), (2, 3))
 
-plot1.set_title("Height")
-plot1.set_xlabel("time (ms)")
-plot1.set_ylabel("height (m)")
-plot1.plot(time, height, color="red")
 
-plot2.set_title("Pressure")
-plot2.set_xlabel("time (ms)")
-plot2.set_ylabel("pressure (pas)")
-plot2.plot(time, pressure, color="red")
+plotHeight.set_title("Height")
+plotHeight.set_xlabel("time (ms)")
+plotHeight.set_ylabel("height (m)")
+plotHeight.plot(time, height, color="red")
+
+plotPressure.set_title("Pressure")
+plotPressure.set_xlabel("time (ms)")
+plotPressure.set_ylabel("pressure (pas)")
+plotPressure.plot(time, pressure, color="darkblue")
+
+plotUV.set_title("UV")
+plotUV.set_xlabel("time (ms)")
+plotUV.set_ylabel("UVI")
+plotUV.plot(time, UV, color="magenta")
+
+
+#----------------------
+plotTempBMP.set_title("TemperatureBMP")
+plotTempBMP.set_xlabel("temperature (°C)")
+plotTempBMP.set_ylabel("time (ms)")
+plotTempBMP.plot(time,temperatureBMP, color="blue")
+
+plotTempMPU.set_title("TemperatureMPU")
+plotTempMPU.set_xlabel("temperature (°C)")
+plotTempMPU.set_ylabel("time (ms)")
+plotTempMPU.plot(time,temperatureMPU, color="blue")
+
+
+#----------------------
+plotAcX.set_title("AcX")
+plotAcX.set_xlabel("time (ms)")
+plotAcX.set_ylabel("x")
+plotAcX.plot(time,AcX, color="green")
+
+plotAcY.set_title("AcY")
+plotAcY.set_xlabel("time (ms)")
+plotAcY.set_ylabel("y")
+plotAcY.plot(time,AcY, color="green")
+
+plotAcZ.set_title("AcZ")
+plotAcZ.set_xlabel("time (ms)")
+plotAcZ.set_ylabel("z")
+plotAcZ.plot(time,AcZ, color="green")
+
+
+#----------------------
+plotGyX.set_title("GyX")
+plotGyX.set_xlabel("time (ms)")
+plotGyX.set_ylabel("x")
+plotGyX.plot(time,GyX, color="yellow")
+
+plotGyY.set_title("GyY")
+plotGyY.set_xlabel("time (ms)")
+plotGyY.set_ylabel("y")
+plotGyY.plot(time,GyY, color="yellow")
+
+plotGyZ.set_title("GyZ")
+plotGyZ.set_xlabel("time (ms)")
+plotGyZ.set_ylabel("z")
+plotGyZ.plot(time,GyZ, color="yellow")
+
 
 plt.tight_layout()
 plt.show()
